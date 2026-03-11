14ce6fb40854c769aa145d72d9bbd13a0d2c8e24
Alongkorn <jiwalongkornn@gmail.com>
2026-03-11 16:24:47 +0700

    อัปเดต Vite config แก้ warning chunk size

--- web-app/vite.config.js
+++ web-app/vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // ขยายลิมิตการเตือนจาก 500 KB เป็น 1000 KB (1 MB)
+    chunkSizeWarningLimit: 1000,
+
+    // แยกไฟล์ Library (Firebase/React) ออกจากไฟล์โค้ดของเรา (Code Splitting)
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
